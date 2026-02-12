57942389fcadb69c98fbfbae6788a52ca6a529f3
reddy9019 <bthimmareddy548@gmail.com>
2026-02-12 17:24:23 +0530

    fixed backend module import

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from backend.main import predict_output
 
 import streamlit as st
 import sys
